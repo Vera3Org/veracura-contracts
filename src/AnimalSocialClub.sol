b861e4f90fcc2d4ee0ca0927ef885b4d06e99655
Veracura <veracurainhabit@gmail.com>
2024-06-29 20:03:55 +0200

    Implemented distribution model.
    TODO:
    - Implement waitlist
    - Implement lottery using Chainlink VRF
    - Review and test auction code
    
    Squashed commit of the following:
    
    commit 93f4085565d7687097ebdd5b62ea82642b668f37
    Author: Veracura <veracurainhabit@gmail.com>
    Date:   Sat Jun 29 19:57:26 2024 +0200
    
        Review code for distribution model
    
    commit 38e0267f38375152348981839ddfaaa46de5d100
    Author: Veracura <veracurainhabit@gmail.com>
    Date:   Sat Jun 29 18:26:34 2024 +0200
    
        fix reserved nft allocation
    
    commit 192d0fe1995ea88ddbcf10a6f0bd5674e42b8db6
    Author: Veracura <veracurainhabit@gmail.com>
    Date:   Sat Jun 29 16:50:20 2024 +0200
    
        move code
    
    commit f55efe55ff963017bc5a4b56feec1f3bdba390a5
    Author: Veracura <veracurainhabit@gmail.com>
    Date:   Sun Jun 23 19:10:30 2024 +0200
    
        implement distribution model for mintElephant. TODO fix tests & review code

--- src/AnimalSocialClub.sol
+++ src/AnimalSocialClub.sol
@@ -5,13 +5,13 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+import "forge-std/console.sol";
 import "forge-std/console2.sol";
 
 contract AnimalSocialClub is ERC1155, Ownable {
     using Strings for uint256;
 
     // Token ID constants
-    uint256 public constant ID_RESERVED = 0;
     uint256 public constant ID_ELEPHANT = 1;
     uint256 public constant ID_SHARK = 2;
     uint256 public constant ID_EAGLE = 3;
@@ -45,6 +45,45 @@ contract AnimalSocialClub is ERC1155, Ownable {
     // Events
     event SaleStateChanged(bool active);
 
+    // Errors
+    error NotAnAmbassador(address account);
+    error NotAnAdvocate(address account);
+    error NotAnEvangelist(address account);
+
+    // Roles mapping
+    enum Role {
+        None,
+        Ambassador,
+        Advocate,
+        Evangelist
+    }
+
+    mapping(address => Role) public roles;
+
+    // Mappings to keep track of hierarchical relationships
+    mapping(address => address[]) public ambassadorToAdvocates;
+    mapping(address => address) public advocateToAmbassador;
+
+    mapping(address => address[]) public advocateToEvangelists;
+    mapping(address => address) public evangelistToAdvocate;
+
+    // Mapping to track Promoter Ambassador, Advocates, and their commissions
+    // commission that one ambassador gives to their advocates. 10 means 10% to advocate, rest to ambassador
+    mapping(address => uint256) public ambassadorToAdvocateCommission;
+    // commission that one advocate gives to their evangelists.
+    mapping(address => uint256) public advocateToEvangelistCommission;
+
+    // Events for role assignment and commission updates
+    event RoleAssigned(address indexed user, Role role);
+    event AmbassadorCommissionSet(
+        address indexed ambassador,
+        uint256 commission_pct
+    );
+    event AdvocateCommissionSet(
+        address indexed advocate,
+        uint256 commission_pct
+    );
+
     // Constructor initializing the ERC-1155 contract with a base URI and beneficiary addresses
     constructor(
         string memory baseURI,
@@ -56,14 +95,72 @@ contract AnimalSocialClub is ERC1155, Ownable {
         ascAddress = _ascAddress;
 
         // Mint reserved tokens for the team
-        _mint(msg.sender, ID_RESERVED, TOTAL_RESERVED, "");
-        tokenSupply[ID_RESERVED] = TOTAL_RESERVED;
+        _mint(msg.sender, ID_ELEPHANT, TOTAL_RESERVED, "");
+        tokenSupply[ID_ELEPHANT] = TOTAL_RESERVED;
+    }
+
+    modifier onlyAmbassador() {
+        if (roles[msg.sender] != Role.Ambassador) {
+            revert NotAnAmbassador(msg.sender);
+        }
+        _;
+    }
+
+    modifier onlyAdvocate() {
+        if (roles[msg.sender] != Role.Advocate) {
+            revert NotAnAdvocate(msg.sender);
+        }
+        _;
+    }
+
+    modifier onlyEvangelist() {
+        if (roles[msg.sender] != Role.Evangelist) {
+            revert NotAnEvangelist(msg.sender);
+        }
+        _;
     }
 
     // Modifier to check if sale is active
     modifier isSaleActive() {
         require(saleActive, "Sale is not active");
         _;
+    }
+
+    // reviewed
+    function assignRole(address user, Role role, address delegate) external {
+        bool isAuthorized = msg.sender == owner();
+
+        if (role == Role.Ambassador) {
+            // here `user` is the owner, and `delegate` is the advocate
+            // only the owner can set an advocate
+            isAuthorized = true;
+        } else if (role == Role.Advocate) {
+            require(
+                roles[user] == Role.Ambassador,
+                "user is not an Ambassador and cannot delegate an Advocate"
+            );
+            isAuthorized = true;
+            // add advocate to the list of the corresponding ambassador
+            ambassadorToAdvocates[user].push(delegate);
+            // reverse the many-to-one mapping
+            advocateToAmbassador[delegate] = user;
+        } else if (role == Role.Evangelist) {
+            require(
+                roles[user] == Role.Advocate,
+                "user is not an Advocate and cannot delegate an Evangelist"
+            );
+            isAuthorized = true;
+            advocateToEvangelists[user].push(delegate);
+            evangelistToAdvocate[delegate] = user;
+        } else if (role == Role.None) {
+            require(
+                msg.sender == owner(),
+                "only the owner can assign arbitrary roles"
+            );
+        }
+        require(isAuthorized, "user not authorized");
+        roles[delegate] = role;
+        emit RoleAssigned(user, role);
     }
 
     // Function to set the base URI for the metadata
@@ -77,8 +174,21 @@ contract AnimalSocialClub is ERC1155, Ownable {
         emit SaleStateChanged(_saleActive);
     }
 
+    function checkReferrer(address referrer) internal view {
+        require(
+            roles[referrer] == Role.Ambassador ||
+                roles[referrer] == Role.Advocate ||
+                roles[referrer] == Role.Evangelist,
+            "referrer must be a valid Ambassador, Advocate or Evangelist"
+        );
+    }
+
     // Function to mint Elephant NFTs
-    function mintElephant(uint256 amount) external payable isSaleActive {
+    function mintElephant(
+        uint256 amount,
+        address referrer
+    ) external payable isSaleActive {
+        checkReferrer(referrer);
         require(
             amount > 0 && amount <= MAXIMUM_MINTABLE,
             "Cannot mint more than MAXIMUM_MINTABLE at a time"
@@ -92,12 +202,20 @@ contract AnimalSocialClub is ERC1155, Ownable {
             "Incorrect ETH amount sent"
         );
 
+        sendCommission(referrer);
+
+        // Mint the NFTs to the buyer
         _mint(msg.sender, ID_ELEPHANT, amount, "");
+
+        // Update token supply
         tokenSupply[ID_ELEPHANT] += amount;
     }
 
-    // Function to mint Shark NFTs
-    function mintShark(uint256 amount) external payable isSaleActive {
+    function mintShark(
+        uint256 amount,
+        address referrer
+    ) external payable isSaleActive {
+        checkReferrer(referrer);
         require(
             amount > 0 && amount <= MAXIMUM_MINTABLE,
             "Cannot mint more than MAXIMUM_MINTABLE at a time"
@@ -108,12 +226,18 @@ contract AnimalSocialClub is ERC1155, Ownable {
         );
         require(msg.value == amount * SHARK_PRICE, "Incorrect ETH amount sent");
 
+        sendCommission(referrer);
+
         _mint(msg.sender, ID_SHARK, amount, "");
         tokenSupply[ID_SHARK] += amount;
     }
 
     // Function to mint EAGLE NFTs
-    function mintEagle(uint256 amount) external payable isSaleActive {
+    function mintEagle(
+        uint256 amount,
+        address referrer
+    ) external payable isSaleActive {
+        checkReferrer(referrer);
         require(
             amount > 0 && amount <= MAXIMUM_MINTABLE,
             "Cannot mint more than MAXIMUM_MINTABLE at a time"
@@ -123,6 +247,8 @@ contract AnimalSocialClub is ERC1155, Ownable {
             "Exceeds total supply of EAGLE tokens"
         );
         require(msg.value == amount * EAGLE_PRICE, "Incorrect ETH amount sent");
+
+        sendCommission(referrer);
 
         _mint(msg.sender, ID_EAGLE, amount, "");
         tokenSupply[ID_EAGLE] += amount;
@@ -141,9 +267,117 @@ contract AnimalSocialClub is ERC1155, Ownable {
         console2.log("Asc3share: ", ascShare);
 
         payable(vera3Address).transfer(vera3Share);
-        console2.log("transfered to vera3");
+        console2.log("transfered veraShare %d to vera3", vera3Share);
         payable(ascAddress).transfer(ascShare);
-        console2.log("transfered to asc");
+        console2.log("transfered ascShare %d to asc", ascShare);
+    }
+
+    function sendCommission(address referrer) internal {
+        checkReferrer(referrer);
+        // Ensure referrer is registered as Ambassador, Advocate
+        require(
+            roles[referrer] == Role.Ambassador ||
+                roles[referrer] == Role.Advocate ||
+                roles[referrer] == Role.Evangelist,
+            "Referrer is not registered as Ambassador, Advocate or Evangelist"
+        );
+
+        // Calculate commissions
+        uint256 totalCommission = msg.value / 10; // 10% commission to Promoter
+
+        // Track commission delegation
+        address ambassador = address(0);
+        address advocate = address(0);
+        address evangelist = address(0);
+
+        if (roles[referrer] == Role.Ambassador) {
+            // Referrer is an Ambassador, all commission goes to them
+            ambassador = referrer;
+            payable(ambassador).transfer(totalCommission);
+        } else if (roles[referrer] == Role.Advocate) {
+            // Referrer is an Advocate delegated by an Ambassador
+            advocate = referrer;
+            ambassador = advocateToAmbassador[advocate];
+            uint256 advocateCommissionPct = ambassadorToAdvocateCommission[
+                ambassador
+            ];
+            uint256 advocateShare = (totalCommission * advocateCommissionPct) /
+                100;
+            uint256 ambassadorShare = totalCommission - advocateShare;
+            require(
+                totalCommission == (ambassadorShare + advocateShare),
+                "Error in calculation for advocate: shares don't add up"
+            );
+            payable(ambassador).transfer(ambassadorShare);
+            payable(advocate).transfer(advocateShare);
+        } else if (roles[referrer] == Role.Evangelist) {
+            evangelist = referrer;
+            advocate = evangelistToAdvocate[evangelist];
+            ambassador = advocateToAmbassador[advocate];
+            // get share % for advocate & evangelist
+            uint256 advocateCommissionPct = ambassadorToAdvocateCommission[
+                ambassador
+            ];
+            uint256 evangelistCommissionPct = advocateToEvangelistCommission[
+                advocate
+            ];
+
+            // calculate advocate & evangelist share in coins
+            uint256 advocateShare = (totalCommission * advocateCommissionPct) /
+                100;
+            uint256 ambassadorShare = totalCommission - advocateShare;
+
+            // the evangelist takes a piece of the advocate's share
+            uint256 evangelistShare = (advocateShare *
+                evangelistCommissionPct) / 100;
+            advocateShare -= evangelistShare;
+
+            require(
+                totalCommission ==
+                    (ambassadorShare + advocateShare + evangelistShare),
+                "Error in calculation for evangelist: shares don't add up"
+            );
+
+            payable(ambassador).transfer(ambassadorShare);
+            payable(advocate).transfer(advocateShare);
+            payable(evangelist).transfer(evangelistShare);
+        } else {
+            revert("referrer role is None!!");
+        }
+    }
+
+    // Function to set commission percentage for Promoter Ambassadors
+    function setAmbassadorToAdvocateCommission(
+        address ambassador,
+        uint256 commissionPercentage
+    ) external {
+        require(
+            roles[msg.sender] == Role.Ambassador || msg.sender == owner(),
+            "Not authorized!"
+        );
+        require(
+            commissionPercentage <= 100,
+            "Commission percentage must be <= 100"
+        );
+        ambassadorToAdvocateCommission[ambassador] = commissionPercentage;
+        emit AmbassadorCommissionSet(ambassador, commissionPercentage);
+    }
+
+    // Function to set commission percentage for Promoter Ambassadors
+    function setAdvocateToEvangelistCommission(
+        address advocate,
+        uint256 commissionPercentage
+    ) external {
+        require(
+            roles[msg.sender] == Role.Advocate || msg.sender == owner(),
+            "Not authorized!"
+        );
+        require(
+            commissionPercentage <= 100,
+            "Commission percentage must be <= 100"
+        );
+        advocateToEvangelistCommission[advocate] = commissionPercentage;
+        emit AdvocateCommissionSet(advocate, commissionPercentage);
     }
 
     // Override URI function to return token-specific metadata
@@ -183,7 +417,7 @@ contract AnimalSocialClub is ERC1155, Ownable {
     }
 
     function placeBid(uint256 i) external payable {
-        require(i < TOTAL_TIGER, "Invalid i");
+        require(i < TOTAL_TIGER, "Invalid card ID");
         require(auctionStarted, "Auction not yet started");
         require(
             !auctionEnded && block.timestamp <= auctionEndTime,
@@ -208,7 +442,7 @@ contract AnimalSocialClub is ERC1155, Ownable {
     }
 
     function endAuction(uint256 i) external onlyOwner {
-        require(i < TOTAL_TIGER, "Invalid i");
+        require(i < TOTAL_TIGER, "Invalid card ID");
         require(auctionStarted, "Auction not yet started");
         require(!auctionEnded, "Auction already ended");
         require(
--- test/AnimalSocialClub.t.sol
+++ test/AnimalSocialClub.t.sol
@@ -13,33 +13,74 @@ contract AnimalSocialClubTest is Test {
     address public user = address(0x5678);
     address public owner = vera3Address;
 
+    address ambassador;
+    address advocate;
+    address evangelist;
+    address buyer;
+
     function setUp() public {
         vm.startPrank(owner);
         asc = new AnimalSocialClub("ipfs://baseURI/", vera3Address, ascAddress);
+        ambassador = vm.addr(1);
+        advocate = vm.addr(2);
+        evangelist = vm.addr(3);
+        buyer = vm.addr(4);
+
+        // Assign roles
+        asc.assignRole(
+            address(0),
+            AnimalSocialClub.Role.Ambassador,
+            ambassador
+        );
+        asc.assignRole(ambassador, AnimalSocialClub.Role.Advocate, advocate);
+        asc.assignRole(advocate, AnimalSocialClub.Role.Evangelist, evangelist);
+
+        // Set commissions
+        asc.setAmbassadorToAdvocateCommission(ambassador, 50); // 50% for this Ambassador
+        asc.setAdvocateToEvangelistCommission(advocate, 50); // 50% for this Advocate
+
+        asc.setSaleActive(true);
         vm.stopPrank();
     }
 
     function testReservedTokens() public view {
-        assertEq(asc.uri(asc.ID_RESERVED()), "ipfs://baseURI/0.json");
-        assertEq(asc.tokenSupply(asc.ID_RESERVED()), asc.TOTAL_RESERVED()); // 250 reserved tokens
+        assertEq(asc.uri(asc.ID_ELEPHANT()), "ipfs://baseURI/1.json");
+        assertEq(asc.tokenSupply(asc.ID_ELEPHANT()), asc.TOTAL_RESERVED()); // 250 reserved tokens
     }
 
     function testMintElephant() public {
+        uint256 ambassadorInitialBalance = ambassador.balance;
+        uint256 initalSupply = asc.tokenSupply(asc.ID_ELEPHANT());
+
         vm.prank(owner);
         asc.setSaleActive(true);
         vm.prank(user);
         vm.deal(user, 2 ether);
-        asc.mintElephant{value: 0.1 ether}(1);
+        asc.mintElephant{value: 0.1 ether}(1, ambassador);
         vm.prank(user);
-        asc.mintElephant{value: 0.1 ether}(1);
+        asc.mintElephant{value: 0.1 ether}(1, ambassador);
         vm.prank(user);
-        asc.mintElephant{value: 0.1 ether}(1);
+        asc.mintElephant{value: 0.1 ether}(1, ambassador);
 
-        assertEq(asc.balanceOf(user, asc.ID_ELEPHANT()), 3);
-        assertEq(asc.tokenSupply(asc.ID_ELEPHANT()), 3);
+        assertEq(
+            asc.balanceOf(user, asc.ID_ELEPHANT()),
+            3,
+            "Balance of user doesnt match expectation"
+        );
+        assertEq(
+            asc.tokenSupply(asc.ID_ELEPHANT()),
+            initalSupply + 3,
+            "Token supply doesnt match expectation"
+        );
+        assertEq(
+            ambassador.balance,
+            ambassadorInitialBalance + 0.03 ether,
+            "Ambassador did not get proper commission"
+        );
     }
 
     function testWithdrawFunds() public {
+        console.log("owner.balance initial: ", owner.balance);
         // Owner opens sale
         vm.prank(owner);
         asc.setSaleActive(true);
@@ -47,20 +88,101 @@ contract AnimalSocialClubTest is Test {
         // user mints elephant
         vm.prank(user);
         vm.deal(user, 2 ether);
-        asc.mintElephant{value: 0.1 ether}(1);
+        asc.mintElephant{value: 0.1 ether}(1, ambassador);
 
         uint256 initialOwnerBalance = owner.balance;
         uint256 initialASCBalance = ascAddress.balance;
+
+        uint256 ambassadorCommission = 0.1 ether / 10;
+        uint256 contractPredictedBalance = (0.1 ether) - ambassadorCommission;
 
         // owner withdraws funds
         vm.prank(owner);
         asc.withdrawFunds();
 
         console.log("owner.balance: ", owner.balance);
-        assertEq(owner.balance, initialOwnerBalance + (0.1 ether * 30) / 100);
+        console.log("ambassador balance: ", ambassador.balance);
+        console.log("ambassador commission: ", ambassadorCommission);
+        assertEq(
+            owner.balance,
+            initialOwnerBalance + (contractPredictedBalance * 30) / 100
+        );
         assertEq(
             ascAddress.balance,
-            initialASCBalance + (0.1 ether * 70) / 100
+            initialASCBalance + (contractPredictedBalance * 70) / 100
+        );
+    }
+
+    function testAmbassadorReferrer() public {
+        // Buyer mints an Elephant with Ambassador as referrer
+        vm.deal(buyer, 1 ether);
+        vm.startPrank(buyer);
+        asc.mintElephant{value: asc.ELEPHANT_PRICE()}(1, ambassador);
+        vm.stopPrank();
+
+        uint256 ambassadorBalance = ambassador.balance;
+        uint256 expectedCommission = (asc.ELEPHANT_PRICE() * 10) / 100;
+
+        assertEq(
+            ambassadorBalance,
+            expectedCommission,
+            "Ambassador commission is incorrect when they are referrer"
+        );
+    }
+
+    function testAdvocateReferrer() public {
+        // Buyer mints an Elephant with Advocate as referrer
+        vm.deal(buyer, 1 ether);
+        vm.startPrank(buyer);
+        asc.mintElephant{value: asc.ELEPHANT_PRICE()}(1, advocate);
+        vm.stopPrank();
+
+        uint256 totalCommission = (asc.ELEPHANT_PRICE() * 10) / 100;
+        uint256 expectedAmbassadorCommission = (totalCommission * 50) / 100;
+        uint256 expectedAdvocateCommission = totalCommission -
+            expectedAmbassadorCommission;
+
+        assertEq(
+            ambassador.balance,
+            expectedAmbassadorCommission,
+            "Ambassador commission is incorrect when Advocate is referrer"
+        );
+        assertEq(
+            advocate.balance,
+            expectedAdvocateCommission,
+            "Advocate commission is incorrect when they are referrer"
+        );
+    }
+
+    function testEvangelistReferrer() public {
+        // Buyer mints an Elephant with Advocate as referrer
+        vm.deal(buyer, 1 ether);
+        vm.startPrank(buyer);
+        asc.mintElephant{value: asc.ELEPHANT_PRICE()}(1, evangelist);
+        vm.stopPrank();
+
+        uint256 totalCommission = (asc.ELEPHANT_PRICE() * 10) / 100;
+        uint256 expectedAmbassadorCommission = (totalCommission * 50) / 100;
+        uint256 expectedAdvocateCommission = totalCommission -
+            expectedAmbassadorCommission;
+        uint256 expectedEvangelistCommission = (expectedAdvocateCommission *
+            50) / 100;
+        expectedAdvocateCommission -= expectedEvangelistCommission;
+
+        assertEq(
+            ambassador.balance,
+            expectedAmbassadorCommission,
+            "Ambassador commission is incorrect when Evangelist is referrer"
+        );
+        assertEq(
+            advocate.balance,
+            expectedAdvocateCommission,
+            "Advocate commission is incorrect when Evangelist is referrer"
+        );
+        assertEq(
+            evangelist.balance,
+            expectedEvangelistCommission,
+            "Evangelist commission is incorrect when they are referrer"
         );
     }
 }
