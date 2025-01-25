9fce158afe45fbe3c1983cd18f56954c7fdb1914
Veracura <veracurainhabit@gmail.com>
2025-01-25 17:53:22 +0100

    edit StartLottery script

--- script/StartLottery.sol
+++ script/StartLottery.sol
@@ -19,9 +19,13 @@ contract StartLottery is Script {
     address public constant ADMIN_ADDRESS = 0x98A5c7E6eb3DEaf7Db34d14d63D46ec5a5A2f775; // dummy CHANGE THIS FOR MAINNET
     address public constant TREASURY_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; // dummy CHANGE THIS FOR MAINNET
 
-    address public constant ETH_FEE_PROXY_ADDRESS = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65; // base sepolia
-    address public constant LINK_ADDRESS = 0xE4aB69C077896252FAFBD49EFD26B5D171A32410; // base sepolia
-    address public constant VRF_WRAPPER_ADDRESS = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed; // base sepolia
+    // address public constant ETH_FEE_PROXY_ADDRESS = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65; // base sepolia
+    // address public constant LINK_ADDRESS = 0xE4aB69C077896252FAFBD49EFD26B5D171A32410; // base sepolia
+    // address public constant VRF_WRAPPER_ADDRESS = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed; // base sepolia
+
+    address public constant LINK_ADDRESS = 0x779877A7B0D9E8603169DdbD7836e478b4624789; // eth sepolia
+    address public constant VRF_WRAPPER_ADDRESS = 0x195f15F2d49d693cE265b4fB0fdDbE15b1850Cc1; // eth sepolia
+    address public constant ETH_FEE_PROXY_ADDRESS =0xe11BF2fDA23bF0A98365e1A4c04A87C9339e8687; // eth sepolia
 
     uint256 public constant ELEPHANT_ID = 0;
     uint256 public constant TIGER_ID = 1;
@@ -37,7 +41,7 @@ contract StartLottery is Script {
 
         asc = ASC721Manager(
             // payable(0xD992580213E98874deb760C7C05903d2dbF8a21a)
-            payable(0x4F64a1f34F4aF09d0546e7a873BE0f03cD62e1cf)
+            payable(0x6a14b7745dfac89a1fe673d034c2e574d8bf06e4) // asc on eth sepolia
         );
 
         asc.addToLotteryParticipants(
@@ -59,7 +63,22 @@ contract StartLottery is Script {
             0x09D4a3d729B13d1Fadc586D8A5E26FED9F41c43c
         );
 
-        // asc.startLottery{value: 19160418160817313}();
+        asc.addToLotteryParticipants(
+            0x0fdf2350397a2a0fb2eb5ffd4ae0d128779b02f9
+        );
+
+        asc.addToLotteryParticipants(
+            0x52fee296d49672c904170858066ecc5e9908b7ab
+        );
+
+        asc.addToLotteryParticipants(
+            0x0052b3d9519b4a45d796a9513dcf34e6577d0a03
+        );
+
+        asc.addToLotteryParticipants(
+            0xc5ff763d5f545a2eb84a253a986678f02e055e23
+        );
+
         asc.startLottery{value: 0.02 ether}();
 
         vm.stopBroadcast();
